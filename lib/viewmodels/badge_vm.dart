9f82317a5e889b96dca08b9a809c9a952badec18
ryanmiura <ryanmiuracarrasco@hotmail.com>
2026-01-02 15:43:47 -0300

    chore: comment unused badge_vm

--- lib/viewmodels/badge_vm.dart
+++ lib/viewmodels/badge_vm.dart
@@ -1,3 +1,4 @@
+// ESTA VIEWMODEL ESTA EM DESUSO.
 import 'package:flutter/material.dart';
 import '../models/badge.dart' as badge_model;
 
@@ -22,53 +23,53 @@ class BadgeViewModel extends ChangeNotifier {
     try {
       // mock dos dados do crachá
       final mock = {
-      "pessNomeVc": "RYAN MIURA CARRASCO",
-      "alunEmailAlternVc": "",
-      "alunemail": "ryan@gmail.com",
-      "emInstAluemailVc": "r@alunos.utfpr.edu.br",
-      "estCivCodNr": 1,
-      "estCivDescrVc": "Solteiro(a)",
-      "login": "a2465779",
-      "matrbloqstatusNr": 0,
-      "paisNacioVc": "Brasileira",
-      "pessMaeVc": "F",
-      "pessNascDt": 1000,
-      "pessPaiVc": "",
-      "pessSexoCh": "M",
-      "tpBloqCodNr": 0,
-      "tpBloqDescrVc": "",
-      "situacaoPassaporte": 1,
-      "cursos": [
-        {
-          "alCuIdVc": "prdpnp",
-          "cursAbrevVc": "BACH ENG DE SOFTWARE",
-          "unidCodNr": 2,
-          "cursCodNr": 65,
-          "ultimaModificacao": null,
-          "cursNomeVc": "Bacharelado Em Engenharia De Software",
-          "tpCurCodNr": 2,
-          "tpCurDescrVc": "Bacharelado",
-          "sitpCodNr": 0,
-          "sitpDescrVc": "Regular",
-          "alCuAnoingNr": 2022,
-          "alCuPerAnoingNr": 1,
-          "alCuCategpgNr": 0,
-          "nivEnsCursoCodNr": 3,
-          "nivEnsDescrVc": "Ensino Superior",
-          "alCuOrdemNr": 1,
-          "alCuCalouroNr": 0,
-          "alCuColacaoDt": null,
-          "alCuPeriodoNr": 4,
-          "gradCodNr": 175,
-          "gradDescrVc": "Bacharelado Em Engenharia De Software",
-          "alCuCoefNr": 0.7748,
-          "alCuTurnoCh": "N",
-          "validadeCracha": "2025-12-31",
-          "anoVigente": 2023,
-          "periodoVigente": 2
-        }
-      ]
-    };
+        "pessNomeVc": "RYAN MIURA CARRASCO",
+        "alunEmailAlternVc": "",
+        "alunemail": "ryan@gmail.com",
+        "emInstAluemailVc": "r@alunos.utfpr.edu.br",
+        "estCivCodNr": 1,
+        "estCivDescrVc": "Solteiro(a)",
+        "login": "a2465779",
+        "matrbloqstatusNr": 0,
+        "paisNacioVc": "Brasileira",
+        "pessMaeVc": "F",
+        "pessNascDt": 1000,
+        "pessPaiVc": "",
+        "pessSexoCh": "M",
+        "tpBloqCodNr": 0,
+        "tpBloqDescrVc": "",
+        "situacaoPassaporte": 1,
+        "cursos": [
+          {
+            "alCuIdVc": "prdpnp",
+            "cursAbrevVc": "BACH ENG DE SOFTWARE",
+            "unidCodNr": 2,
+            "cursCodNr": 65,
+            "ultimaModificacao": null,
+            "cursNomeVc": "Bacharelado Em Engenharia De Software",
+            "tpCurCodNr": 2,
+            "tpCurDescrVc": "Bacharelado",
+            "sitpCodNr": 0,
+            "sitpDescrVc": "Regular",
+            "alCuAnoingNr": 2022,
+            "alCuPerAnoingNr": 1,
+            "alCuCategpgNr": 0,
+            "nivEnsCursoCodNr": 3,
+            "nivEnsDescrVc": "Ensino Superior",
+            "alCuOrdemNr": 1,
+            "alCuCalouroNr": 0,
+            "alCuColacaoDt": null,
+            "alCuPeriodoNr": 4,
+            "gradCodNr": 175,
+            "gradDescrVc": "Bacharelado Em Engenharia De Software",
+            "alCuCoefNr": 0.7748,
+            "alCuTurnoCh": "N",
+            "validadeCracha": "2025-12-31",
+            "anoVigente": 2023,
+            "periodoVigente": 2
+          }
+        ]
+      };
 
       _badge = badge_model.Badge.fromJson(mock);
       _state = BadgeState.loaded;
