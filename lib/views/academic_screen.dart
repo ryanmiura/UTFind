75514cd1d4545d69e34b9a41a6e2e5d347f0ed2f
ryanmiura <ryanmiuracarrasco@hotmail.com>
2026-03-31 19:04:09 -0300

    feat: update AcademicScreen to include Bulletin tab and screen

--- lib/views/academic_screen.dart
+++ lib/views/academic_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'history_screen.dart';
 import 'curriculum_screen.dart';
+import 'bulletin_screen.dart';
 
 class AcademicScreen extends StatelessWidget {
   const AcademicScreen({super.key});
@@ -8,12 +9,13 @@ class AcademicScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 2,
+      length: 3,
       child: Scaffold(
         appBar: AppBar(
           title: const Text('Acadêmico'),
           bottom: const TabBar(
             tabs: [
+              Tab(text: 'Boletim', icon: Icon(Icons.assessment)),
               Tab(text: 'Histórico', icon: Icon(Icons.history_edu)),
               Tab(text: 'Matriz', icon: Icon(Icons.table_chart)),
             ],
@@ -21,6 +23,7 @@ class AcademicScreen extends StatelessWidget {
         ),
         body: const TabBarView(
           children: [
+            BulletinScreen(),
             HistoryScreen(),
             CurriculumScreen(),
           ],
